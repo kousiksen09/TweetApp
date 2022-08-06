37900fe832c8589bb1434e7fb7d8a9a8260e752c
Debs5312 <debojyotisaha5@gmail.com>
2022-08-06 13:33:11 +0530

    Testing Done for TweetPOST

--- Test.TweetPostMicroservice/TweetPostAPITest.cs
+++ Test.TweetPostMicroservice/TweetPostAPITest.cs
@@ -60,8 +60,6 @@ namespace Test.TweetPostMicroservice
 
         [Test]
         [TestCase(1)]
-        [TestCase(2)]
-        [Parallelizable(ParallelScope.All)]
         public async Task GetTweetbyIDAsync_giveTweetId_ReturnsTweetById(int id)
         {
             _tweet = new TweetReadDTO() { TweetID = id, Like = id, Caption = "Country", Body = "UK" };
@@ -78,8 +76,6 @@ namespace Test.TweetPostMicroservice
 
         [Test]
         [TestCase(8)]
-        [TestCase(10)]
-        [Parallelizable(ParallelScope.All)]
         public async Task GetTweetbyIDAsync_giveInvalidTweetId_ReturnsNoTweet(int id)
         {
             _repositoryStub.Setup(repo => repo.GetTweetById(id));
@@ -95,7 +91,6 @@ namespace Test.TweetPostMicroservice
 
         [Test]
         [TestCase("sakdjk21lsdknfdslkfj")]
-        [Parallelizable(ParallelScope.All)]
         public async Task GetMyTweetsAsync_giveUserId_ReturnsTweetsByUserId(string userID)
         {
             _listTweets = new List<TweetReadDTO>() {
@@ -115,7 +110,6 @@ namespace Test.TweetPostMicroservice
 
         [Test]
         [TestCase("cndldld2327NKDSADNKS")]
-        [Parallelizable(ParallelScope.All)]
         public async Task GetMyTweetsAsync_InvalidUserId_ReturnNoTweets(string userID)
         {
             _listTweets = new List<TweetReadDTO>();
@@ -128,6 +122,117 @@ namespace Test.TweetPostMicroservice
             Assert.That(contentResult, Is.InstanceOf<ResponseDTO>());
             Assert.AreEqual(_listTweets, (contentResult as ResponseDTO).Result);
             Assert.False((contentResult as ResponseDTO).IsSuccess);
+        }
+
+        [Test]
+        [TestCase("sadafsakdjk21lsdknfdslkfj")]
+        public async Task PostTweetsAsync_giveUserIDandPOST_AddtoDBSuccesfully(string userID)
+        {
+            var postTweet = new TweetUpsertDTO() { Caption="Country", Body="India" };
+            _tweet = new TweetReadDTO() { TweetID = 1, Like = 0, Caption = "Country", Body = "India" };
+            _repositoryStub.Setup(repo => repo.CreateTweet(postTweet, userID)).ReturnsAsync(_tweet);
+
+            var result = await _tweetAPIController.Post(postTweet, userID);
+            var contentResult = (result as ObjectResult).Value;
+
+            Assert.AreEqual((result as ObjectResult).StatusCode, 201);
+            Assert.That(contentResult, Is.InstanceOf<ResponseDTO>());
+            Assert.AreEqual(_tweet, (contentResult as ResponseDTO).Result);
+            Assert.True((contentResult as ResponseDTO).IsSuccess);
+        }
+
+        [Test]
+        [TestCase("sadafsakdjk21lsdknfdslkfj")]
+        public async Task PostTweetsAsync_giveUserIDandPOST_NotAddedToDB(string userID)
+        {
+            var postTweet = new TweetUpsertDTO() { Caption = "Country", Body = "India" };
+            _repositoryStub.Setup(repo => repo.CreateTweet(postTweet, userID));
+
+            var result = await _tweetAPIController.Post(postTweet, userID);
+            var contentResult = (result as ObjectResult).Value;
+
+            Assert.AreEqual(500, (result as ObjectResult).StatusCode);
+            Assert.That(contentResult, Is.InstanceOf<ResponseDTO>());
+            Assert.AreEqual(null, (contentResult as ResponseDTO).Result);
+            Assert.False((contentResult as ResponseDTO).IsSuccess);
+        }
+
+        [Test]
+        [TestCase(2, "sadafsakdjk21lsdknfdslkfj")]
+        public async Task UpdateTweetAsync_giveTweetIDandUserIDandTweet_UpdatedtoDBSuccesfully(int id, string userID)
+        {
+            var postTweet = new TweetUpsertDTO() { Caption = "Country", Body = "Brazil" };
+            _tweet = new TweetReadDTO() { TweetID = id, Like = 0, Caption = "Country", Body = "Brazil" };
+            _repositoryStub.Setup(repo => repo.UpdateTweet(postTweet, id, userID)).ReturnsAsync(_tweet);
+
+            var result = await _tweetAPIController.Put(postTweet, id, userID);
+            var contentResult = (result as ObjectResult).Value;
+
+            Assert.AreEqual((result as ObjectResult).StatusCode, 200);
+            Assert.That(contentResult, Is.InstanceOf<ResponseDTO>());
+            Assert.AreEqual(_tweet, (contentResult as ResponseDTO).Result);
+            Assert.True((contentResult as ResponseDTO).IsSuccess);
+        }
+
+        [Test]
+        [TestCase(2, "sadafsakdjk21lsdknfdslkfj")]
+        public async Task UpdateTweetAsync_giveTweetIDandUserIDandTweet_NotUpdated(int id, string userID)
+        {
+            var postTweet = new TweetUpsertDTO() { Caption = "Country", Body = "Brazil" };
+            _repositoryStub.Setup(repo => repo.UpdateTweet(postTweet, id, userID));
+
+            var result = await _tweetAPIController.Put(postTweet, id, userID);
+            var contentResult = (result as ObjectResult).Value;
+
+            Assert.AreEqual((result as ObjectResult).StatusCode, 404);
+            Assert.That(contentResult, Is.InstanceOf<ResponseDTO>());
+            Assert.AreEqual(null, (contentResult as ResponseDTO).Result);
+            Assert.False((contentResult as ResponseDTO).IsSuccess);
+        }
+
+        [Test]
+        [TestCase(2)]
+        public async Task DeleteTweetAsync_giveTweetID_DeletedtoDBSuccesfully(int id)
+        {
+            _repositoryStub.Setup(repo => repo.DeleteTweet(id)).ReturnsAsync(true);
+
+            var result = await _tweetAPIController.Delete(id);
+            var contentResult = (result as ObjectResult).Value;
+
+            Assert.AreEqual((result as ObjectResult).StatusCode, 200);
+            Assert.That(contentResult, Is.InstanceOf<ResponseDTO>());
+            Assert.AreEqual("Existed Tweet Deleted. ", (contentResult as ResponseDTO).DisplayMessage);
+            Assert.True((contentResult as ResponseDTO).IsSuccess);
+        }
+
+        [Test]
+        [TestCase(2)]
+        public async Task DeleteTweetAsync_giveTweetID_NotFoundTweetForDelete(int id)
+        {
+            _repositoryStub.Setup(repo => repo.DeleteTweet(id)).ReturnsAsync(false);
+
+            var result = await _tweetAPIController.Delete(id);
+            var contentResult = (result as ObjectResult).Value;
+
+            Assert.AreEqual((result as ObjectResult).StatusCode, 404);
+            Assert.That(contentResult, Is.InstanceOf<ResponseDTO>());
+            Assert.AreEqual("Tweet Dosnt exist. Please provide a valid Tweet ID", (contentResult as ResponseDTO).DisplayMessage);
+            Assert.False((contentResult as ResponseDTO).IsSuccess);
+        }
+
+        [Test]
+        [TestCase(3)]
+        public async Task LikeTweetAsync_giveTweetID_LikeAddedSuccesfully(int id)
+        {
+            _repositoryStub.Setup(repo => repo.AddLike(id)).ReturnsAsync(true);
+
+            var result = await _tweetAPIController.Put(id);
+            var contentResult = (result as ObjectResult).Value;
+
+            Assert.AreEqual((result as ObjectResult).StatusCode, 200);
+            Assert.That(contentResult, Is.InstanceOf<ResponseDTO>());
+            Assert.AreEqual("New likes added. ", (contentResult as ResponseDTO).DisplayMessage);
+            Assert.True((contentResult as ResponseDTO).IsSuccess);
         }
 
     }
