b9a312da38313bb96218651bfdebc7806a93f6e2
Sherlock2603 <basudeepjyoti@gmail.com>
2022-08-09 11:40:45 +0530

    Test for Reply Microservice

--- Test.TweetReplyMicroservice/TweetReplyTest.cs
+++ Test.TweetReplyMicroservice/TweetReplyTest.cs
@@ -1,14 +1,26 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TweetView_ReplyMicroservice.Controllers;
+using TweetView_ReplyMicroservice.Repository;
 
 namespace Test.TweetReplyMicroservice
 {
     [TestFixture]
     public class TweetReplyTest
     {
+        private Mock<ITweetReplyRepository> _repositoryStub;
+        private TweetReplyController _tweetReplyController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _repositoryStub = new Mock<ITweetReplyRepository>();
+            _tweetReplyController = new TweetReplyController(_repositoryStub.Object);
+        }
     }
 }
