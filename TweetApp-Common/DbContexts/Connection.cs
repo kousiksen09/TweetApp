9bae7f681b7692020a2bc0189ea357573ea08ead
Sherlock2603 <basudeepjyoti@gmail.com>
2022-08-15 14:11:43 +0530

    ConnectionString

--- TweetApp-Common/DbContexts/Connection.cs
+++ TweetApp-Common/DbContexts/Connection.cs
@@ -5,7 +5,8 @@
         public string ConnectionString { get; set; }
         public Connection()
         {
-           ConnectionString = "data source=KOUSIK-SEN-HP-P\\SQLEXPRESS; Initial Catalog=Tweet_Server;integrated security=true;MultipleActiveResultSets=true";
+           //ConnectionString = "data source=KOUSIK-SEN-HP-P\\SQLEXPRESS; Initial Catalog=Tweet_Server;integrated security=true;MultipleActiveResultSets=true";
+            ConnectionString = "data source=CTSDOTNET376; Initial Catalog=Tweet_Server;integrated security=true;MultipleActiveResultSets=true";
 
         }
     }
